608499d56758e41c40d9b7b104e532c6c7e45e14
isk03300 <isk03300@naver.com>
2024-01-12 12:56:17 +0900

    add

---
+++ app/src/main/java/com/sunny/firebasetest/model/G.java
@@ -0,0 +1,8 @@
+package com.sunny.firebasetest.model;
+
+public class G {
+
+    public static String nickName;
+    public static String profileUri;
+    public static String porfileUrl;
+}
---
+++ app/src/main/java/com/sunny/firebasetest/model/User.java
@@ -0,0 +1,7 @@
+package com.sunny.firebasetest.model;
+
+public class User {
+
+    public static String nickName;
+    public static String profileUri;
+}
